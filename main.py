9ab6fcece8c4e730462569509b2066551de40599
maandreev <ma.andreev@bk.ru>
2021-03-29 00:10:59 +0300

    Reshenia

--- main.py
+++ main.py
@@ -2,18 +2,26 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-
-items = os.getenv('ITEMS')
-sum = os.getenv('SUM')
-order_num = os.getenv('ORDER_NUM')
+# ITEMS(Количесто в заказе) = 3
+# SUM(Сумма заказа) = 100.00
+# ORDER_NUM(Номер в очереди) 265
+items = int(os.getenv('ITEMS'))
+sum = float(os.getenv('SUM'))
+order_num = int(os.getenv('ORDER_NUM'))
 
 # 1. Касса перепутала номер в очереди с суммой. Надо поменять эти переменные местами
+order_num, sum = sum, order_num
+print('ORDER_NUM:', order_num, 'SUM:', sum)
 ...
 
 # 2. Для отчёта боссу, нужно посчитать среднюю стоимость каждой пиццы в заказе
+print('Средняя стоимоть пиццы: ', sum/order_num)
+
 ...
 
 # 3. Если в сумме заказа нету дробей (.00), нужно, чтобы нули не отрисовывались
+order_num = int(100.11)
+print(order_num)
 ...
 
 # 4. Если у клиента в номере заказа есть цифра 2, ему положена скидка в 50%
